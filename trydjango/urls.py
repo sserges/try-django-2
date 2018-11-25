f7daf9a185ee291088b96cfbf2ad61921ce15d8f
sserges <agounkeserge@gmail.com>
2018-11-25 19:02:13 +0000

    Chapter 12 completed

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import (
+	home_view,
+)
+
 urlpatterns = [
+	path('', home_view, name='home'),
     path('admin/', admin.site.urls),
 ]
