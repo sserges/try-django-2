ab7f17f9aaccb92cd3b02a4461de800d2174606f
sserges <agounkeserge@gmail.com>
2018-11-25 20:54:18 +0000

    Chapter 13 completed

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+	print(request,args, kwargs)
+	print(request.user)
 	return HttpResponse("<h1>Hello World</h1>")
